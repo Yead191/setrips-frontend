84983055bc7a04828dd466fa24ba5aa867935b6f
Yead191 <yead191@gmail.com>
2026-04-16 09:02:53 +0600

    updated

--- src/features/(web-pages)/fleet/sections/ChauffeurFleet.tsx
+++ src/features/(web-pages)/fleet/sections/ChauffeurFleet.tsx
@@ -1,3 +1,12 @@
+import {
+    Pagination,
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+} from "@/components/ui/pagination"
 import { CarCard } from "@/components/CarCard";
 import { cars } from "@/constants/fleet/car-data";
 import SectionTitle from "@/shared/SectionTItle";
@@ -6,11 +15,35 @@ export default function ChauffeurFleet() {
     return (
         <section className="container pt-20 lg:pt-24 lg:pb-12 pb-10">
             <SectionTitle title="Our Luxury Chauffeur Fleet" align="center" />
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8 mt-12 lg:mt-16">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8 mt-12 lg:mt-16 mb-8">
                 {cars.map((car) => (
                     <CarCard key={car.id} car={car} />
                 ))}
             </div>
+            <Pagination>
+                <PaginationContent>
+                    <PaginationItem>
+                        <PaginationPrevious href="#" />
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationLink href="#">1</PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationLink href="#" isActive>
+                            2
+                        </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationLink href="#">3</PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationEllipsis />
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationNext href="#" />
+                    </PaginationItem>
+                </PaginationContent>
+            </Pagination>
         </section>
     )
 }
